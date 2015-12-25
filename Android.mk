3fe72dfd5df283b2c23132f3f1658ef4a96bd372
Ethan Chen <intervigil@gmail.com>
2015-12-24 20:22:06 -0800

    media: Use project pathmap
    Change-Id: Id1851644fef3756caa0b9e8774fb68dd90ca4392

--- Android.mk
+++ Android.mk
@@ -7,5 +7,6 @@ include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
+endif
 
 endif
